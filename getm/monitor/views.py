0db4598c874b9cfa5150d61541b2f55287d4c0f8
Rit <jrite09@gmail.com>
2019-06-26 12:59:25 -0400

    started search page

--- getm/monitor/views.py
+++ getm/monitor/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index (request):
     return render(request, "monitor/index.html")
+
+def searching (request):
+    return render(request, "monitor/index.html")
